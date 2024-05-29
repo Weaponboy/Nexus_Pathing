9603ec5b3f3c70ed5631c289fdb21b82a2eb1472
Josh Anderson <111634244+Weaponboy@users.noreply.github.com>
2024-05-29 11:57:10 +0200

    module jitpack added

--- app/src/androidTest/java/Util.java
+++
@@ -1,5 +0,0 @@
-public class Util {
-    public double Return(){
-        return 10;
-    }
-}
--- app/src/test/java/dev/weaponboy/nexus/Util.java
+++
@@ -1,9 +0,0 @@
-package dev.weaponboy.nexus;
-
-public class Util {
-
-    public double Return(){
-        return 10;
-    }
-
-}
