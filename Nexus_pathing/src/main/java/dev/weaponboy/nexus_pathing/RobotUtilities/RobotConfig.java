9b617841dc4c9c026a8fee5e64a3211db6c244b6
Weaponboy <joshandy06@gmail.com>
2024-10-29 21:09:44 +0200

    v1.0.6: Updating I end targeting, got real robot velocities

--- Nexus_pathing/src/main/java/dev/weaponboy/nexus_pathing/RobotUtilities/RobotConfig.java
+++ Nexus_pathing/src/main/java/dev/weaponboy/nexus_pathing/RobotUtilities/RobotConfig.java
@@ -31,10 +31,10 @@ public class RobotConfig {
     double HEADING_P_SMALL = 0.01;
     double HEADING_D_SMALL = 0.0008;
 
-    double MAX_X_VELOCITY = 240;
+    double MAX_X_VELOCITY = 220;
     double MAX_Y_VELOCITY = 180;
 
-    double MAX_X_ACCELERATION = 210;
+    double MAX_X_ACCELERATION = 130;
     double MAX_Y_ACCELERATION = 90;
 
     public RobotConfig() {}
